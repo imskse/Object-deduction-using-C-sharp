fa9c858a21736293c5b86c4bef14796697ff4a94
SKhanal <SKhanal@-LIJOCS0>
2020-12-11 10:15:44 +0900

    .

---
+++ WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboCamera = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cboCamera
+            // 
+            this.cboCamera.FormattingEnabled = true;
+            this.cboCamera.Location = new System.Drawing.Point(297, 12);
+            this.cboCamera.Name = "cboCamera";
+            this.cboCamera.Size = new System.Drawing.Size(121, 20);
+            this.cboCamera.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1000, 570);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1025, 594);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.cboCamera);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox cboCamera;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using Alturos.Yolo;
+using Alturos.Yolo.Model;
+using Microsoft.ML;
+using Tensorflow.Data;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        FilterInfoCollection filterInfoCollection;
+        VideoCaptureDevice videoCaptureDevice;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Start()
+        {
+            videoCaptureDevice = new VideoCaptureDevice(filterInfoCollection[cboCamera.SelectedIndex].MonikerString);
+            //videoCaptureDevice.DesiredFrameRate = 20;
+            videoCaptureDevice.NewFrame += VideoCaptureDevice_NewFrame;
+            videoCaptureDevice.Start();
+        }
+
+        private void VideoCaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            //Bitmap sss = new Bitmap(1000,700);
+            //Rectangle cloneRect = new Rectangle(0, 0, 1000, 700);
+            //PixelFormat format = new sss.PixelFormat();
+            pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+            
+            Dedect();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo Device in filterInfoCollection)
+                cboCamera.Items.Add(Device.Name);
+            cboCamera.SelectedIndex = 0;
+            videoCaptureDevice = new VideoCaptureDevice();
+            Start();
+        }  
+
+        private void Dedect()
+        {
+            var ConfigurationDetector = new ConfigurationDetector();
+            var config = ConfigurationDetector.Detect();
+            var yolo = new YoloWrapper(config);
+            var memoryStream = new MemoryStream();
+            pictureBox1.Image.Save(memoryStream, ImageFormat.Png);
+            var _items = yolo.Detect(memoryStream.ToArray()).ToList();
+            AddDetailsToPicture(pictureBox1, _items); 
+        }
+
+        private void AddDetailsToPicture(PictureBox pictureBox1, List<YoloItem> items)
+        {
+            var img = pictureBox1.Image;
+            var font = new Font("Arial", 18, FontStyle.Bold);
+            var brush = new SolidBrush(Color.Red);
+            var graphics = Graphics.FromImage(img);
+            int i = 0;
+            foreach (var item in items)
+            {
+                var x = items[i].X;
+                var y = items[i].Y;
+                var width = item.Width;
+                var hight = item.Height;
+                var tung = item.Type;
+                var rect = new Rectangle(x, y, width, hight);
+                var pen = new Pen(Color.LightGreen, 6);
+                var Point = new Point(x, y);
+
+                graphics.DrawRectangle(pen, rect);
+                graphics.DrawString(item.Type, font, brush, Point);
+                i++;
+            }
+            pictureBox1.Image = img;
+        }
+
+    }
+}
